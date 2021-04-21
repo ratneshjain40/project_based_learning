fc7f9c87a02706b94cc1220585abbf083a3329c3
Ratnesh Jain <ratneshjain40@gmail.com>
2021-04-21 14:13:37 +0530

    Lab_5_basic_done

--- Lab_5/Program_1.java
+++ Lab_5/Program_1.java
@@ -8,6 +8,8 @@ abstract class Shapes {
     private Boolean filled;
 
     Shapes() {
+        this.color = "No Color";
+        this.filled = false;
     }
 
     Shapes(String color, Boolean filled) {
@@ -47,10 +49,8 @@ abstract class Shapes {
 class Circle extends Shapes {
     private double radius;
 
-    Circle() {
-    }
-
     Circle(double radius) {
+        super();
         this.radius = radius;
     }
 
@@ -82,10 +82,8 @@ class Rectangle extends Shapes {
     private double width;
     private double length;
 
-    Rectangle() {
-    }
-
     Rectangle(double length, double width) {
+        super();
         this.length = length;
         this.width = width;
 
@@ -125,7 +123,13 @@ class Rectangle extends Shapes {
 }
 
 class Square extends Rectangle {
-    Square() {
+
+    Square(double side) {
+        super(side, side);
+    }
+
+    Square(double side, String color, Boolean filled) {
+        super(side, side, color, filled);
     }
 
     public void setSide(double side) {
@@ -152,6 +156,10 @@ class Square extends Rectangle {
 
 public class Program_1 {
     public static void main(String[] args) {
+        Circle circle = null;
+        Rectangle rectangle = null;
+        Square square = null;
+
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         boolean loop = true;
         try {
@@ -164,9 +172,61 @@ public class Program_1 {
                 int ch = Integer.parseInt(input.readLine());
                 switch (ch) {
                 case 1: {
+                    System.out.println("1. Circle");
+                    System.out.println("2. Rectange");
+                    System.out.println("3. Square");
+
+                    System.out.print("Enter Choice : ");
+                    String sh = input.readLine();
+
+                    if (sh.equals("1")) {
+                        System.out.print("Enter Radius : ");
+                        double rad = Integer.parseInt(input.readLine());
+
+                        Circle cir = new Circle(rad);
+                        circle = cir;
+                    } else if (sh.equals("2")) {
+                        System.out.print("Enter Length : ");
+                        double length = Integer.parseInt(input.readLine());
+                        System.out.print("Enter Width : ");
+                        double width = Integer.parseInt(input.readLine());
+
+                        Rectangle rect = new Rectangle(length, width);
+                        rectangle = rect;
+                    } else if (sh.equals("3")) {
+                        System.out.print("Enter Side : ");
+                        double length = Integer.parseInt(input.readLine());
+
+                        Square squ = new Square(length);
+                        square = squ;
+                    } else {
+                        System.out.println("Not an Option !");
+                    }
+
                     break;
                 }
                 case 2: {
+                    if (circle != null) {
+                        System.out.println("Shape : Circle");
+                        System.out.println("Color : " + circle.getColor());
+                        System.out.println("Filled : " + circle.getFilled());
+                        System.out.println("Area : " + circle.getArea());
+                        System.out.println("Peremeter: " + circle.getPeremeter());
+                    }
+                    if (rectangle != null) {
+                        System.out.println("Shape : Rectangle");
+                        System.out.println("Color : " + rectangle.getColor());
+                        System.out.println("Filled : " + rectangle.getFilled());
+                        System.out.println("Area : " + rectangle.getArea());
+                        System.out.println("Peremeter: " + rectangle.getPeremeter());
+                    }
+                    if (square != null) {
+                        System.out.println("Shape : Square");
+                        System.out.println("Color : " + square.getColor());
+                        System.out.println("Filled : " + square.getFilled());
+                        System.out.println("Area : " + square.getArea());
+                        System.out.println("Peremeter: " + square.getPeremeter());
+                    }
                     break;
                 }
                 default: {
